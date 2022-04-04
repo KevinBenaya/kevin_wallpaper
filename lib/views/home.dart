d2b8c22c38932f226da48cf0c4739955b347292c
KevinBenaya <darkfaust063@gmail.com>
2022-04-04 10:32:25 +0700

    Adding some codes for home.dart

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:kevin_wallpaper/data/data.dart';
 import 'package:kevin_wallpaper/widgets/widget.dart';
+import 'package:kevin_wallpaper/model/categories_model.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,6 +11,14 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<CategoriesModel> categories = new List();
+
+  @override
+  void initState() {
+    categories = getCategories();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,8 +51,64 @@ class _HomeState extends State<Home> {
                 ],
               ),
             ),
+            SizedBox(
+              height: 16,
+            ),
+            Container(
+              height: 80,
+              child: ListView.builder(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                itemCount: categories.length,
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return CategoriesTile(
+                    title: categories[index].categorieName,
+                    imgUrl: categories[index].imgUrl,
+                  );
+                },
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CategoriesTile extends StatelessWidget {
+  final String imgUrl, title;
+  CategoriesTile({@required this.title, @required this.imgUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 4),
+      child: Stack(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.network(
+              imgUrl,
+              height: 50,
+              width: 100,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            color: Colors.black26,
+            height: 50,
+            width: 100,
+            alignment: Alignment.center,
+            child: Text(
+              title,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 15),
+            ),
+          ),
+        ],
       ),
     );
   }
