f99281ca47d76cee98da3ef0d173247114a7b57d
KevinBenaya <darkfaust063@gmail.com>
2022-04-05 09:52:36 +0700

    Fix some errors

--- lib/model/categories_model.dart
+++ lib/model/categories_model.dart
@@ -1,13 +1,4 @@
 class CategoriesModel {
-  String categorieName;
-  String imgUrl;
-
-  //CategoriesModel({required this.categorieName, required this.imgUrl});
-
-  /*factory CategoriesModel.fromMap(Map<String, dynamic> jsonData) {
-    return CategoriesModel(
-        categorieName: jsonData["categorieName"],
-        imgUrl: jsonData["imgUrl"],
-
-  }*/
+  String? categorieName;
+  String? imgUrl;
 }
